a9db37d2249d9ade41e0ec09df73d3d7ff41587a
BobitanDenisa <45236028+BobitanDenisa@users.noreply.github.com>
2020-03-04 22:13:32 +0200

    Add files via upload

---
+++ TrafficSimulation/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import controller.SimulationManager;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimulationManager simulation = new SimulationManager();
+    }
+
+}
